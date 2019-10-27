c9add736d0a082ebd93230170cd9be6a25b0f443
Santiago <santyf@live.com>
2019-10-27 13:22:51 -0400

    trying to animate gifs

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ var topics = ["Sports", "Dogs", "Gaming", "Music"];
 
 function displayInfo() {
 
-    
+
     var name = $(this).attr("data-name");
     var APIKey = "IcqtslR9hjOS6kk8EC1CldDCskqZoWYT";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=" + APIKey + "&limit=10";
@@ -20,28 +20,32 @@ function displayInfo() {
         console.log(queryURL);
 
         for (var i = 0; i < response.data.length; i++) {
-            
+
             var inputDiv = $("<div>");
 
-            inputDiv.addClass("gifs");
 
-        
             var rating = response.data[i].rating;
 
-        
+
             var pRating = $("<p>").text("Rating: " + rating);
 
             var imgURL = response.data[i].images['480w_still'].url;
-            
+
             var image = $("<img>").attr("src", imgURL);
-            
-            
+
+            inputDiv.attr("src", response[i].images.original_still.url);
+            inputDiv.attr("data-still", response[i].images.original_still.url);
+            inputdiv.attr("data-animate", response[i].images.original.url);
+            inputDiv.attr("data-state", "still");
+            inputDiv.addClass("gifs");
+           
+
             inputDiv.append(pRating);
 
-            
+
             inputDiv.append(image);
-            
-            
+
+
             $("#gifs-view").append(inputDiv);
         }
     });
@@ -50,22 +54,24 @@ function displayInfo() {
 
 function renderButtons() {
 
-  
+
     $("#buttons-view").empty();
 
-    
+
     for (var i = 0; i < topics.length; i++) {
 
-       
+
         var a = $("<button>");
-        
+
         a.addClass("gif-btn");
-        
+
         a.attr("data-name", topics[i]);
-    
+
         a.text(topics[i]);
-       
+
         $("#buttons-view").append(a);
+
+
     }
 }
 
@@ -82,8 +88,23 @@ $("#add-gif").on("click", function (event) {
     renderButtons();
 });
 
+function changeState() {
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillImage = $(this).attr("data-still");
+
+    if (state == "still") {
+        $(this).attr("src", animateImage);
+        $(this).attr("data-state", "animate");
+    }
+
+    else if (state == "animate") {
+        $(this).attr("src", stillImage);
+        $(this).attr("data-state", "still");
+    }
+}
 
 $(document).on("click", ".gif-btn", displayInfo);
-
+$(document).on("click", ".gifs", changeState);
 
 renderButtons();
